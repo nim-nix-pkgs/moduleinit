9b84ffc3e9a9e0603a2ccb37666283d6a8ae69b7
hugosenari <hugosenari@gmail.com>
2022-03-12 03:30:23 -0300

    chore: re index moduleinit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = ''Nim module/thread initialisation ordering library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."moduleinit-master".url = "path:./master";
+  inputs."moduleinit-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."moduleinit-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."moduleinit-v0_1_0".url = "path:./v0_1_0";
+  inputs."moduleinit-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."moduleinit-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."moduleinit-v0_2_0".url = "path:./v0_2_0";
+  inputs."moduleinit-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."moduleinit-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."moduleinit-v0_3_0".url = "path:./v0_3_0";
+  inputs."moduleinit-v0_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."moduleinit-v0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."moduleinit-v0_4_0".url = "path:./v0_4_0";
+  inputs."moduleinit-v0_4_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."moduleinit-v0_4_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim module/thread initialisation ordering library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-moduleinit-master.flake = false;
+  inputs.src-moduleinit-master.owner = "skunkiferous";
+  inputs.src-moduleinit-master.ref   = "refs/heads/master";
+  inputs.src-moduleinit-master.repo  = "moduleinit";
+  inputs.src-moduleinit-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-moduleinit-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-moduleinit-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
